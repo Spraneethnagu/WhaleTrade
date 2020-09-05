88ee3a6abd8f0df73cd512ed5dc926b17fbc0659
Amartya Nigam <amartyanigam6@gmail.com>
2020-09-05 14:28:53 +0530

    Add Links in Resources

--- RssFeed/views.py
+++ RssFeed/views.py
@@ -113,120 +113,213 @@ def index(request):
 
 def resources(request):
     c_wallets=[
-    {'name': "My Ether Wallet"},
-    {'name': "Blockchani Wallet"},
-    {'name': "Cryptonator Online Wallet"},
-    {'name': "Coinbase Wallet"},
-    {'name': "Btc.com Wallet"}
+    {'name': "My Ether Wallet",
+     'link': "https://www.myetherwallet.com/"},
+    {'name': "Blockchani Wallet",
+     'link': "https://www.blockchain.com/wallet"},
+    {'name': "Cryptonator Online Wallet",
+     'link': "https://www.cryptonator.com/"},
+    {'name': "Coinbase Wallet",
+     'link': "https://wallet.coinbase.com/"},
+    {'name': "Btc.com Wallet",
+     'link': "https://wallet.btc.com/"}
     
     ]
 
     h_wallets=[
-        {'name': "Ledger"},
-        {'name': "Trezor"},
-        {'name': "CoolWallet"},
-        {'name': "BitLox"},
-        {'name': "KeepKey"}
-        
-        ]
-
-    c_gambling = [
-        {'name': "Luckygames.io"},
-        {'name': "bitStarz"},
-        {'name': "Nitrogensports"},
-        {'name': "Primedice"},
-        {'name': "mBit Casino"}
-        
+        {'name': "Ledger",
+         'link': "https://www.ledger.com"},
+        {'name': "Trezor",
+        'link': "https://trezor.io/"},
+        {'name': "CoolWallet",
+         'link': "https://www.coolwallet.io/"},
+        {'name': "BitLox",
+         'link': "https://www.bitlox.com/"},
+        {'name': "KeepKey",
+         'link': "https://shapeshift.com/keepkey"}
         
         ]
 
     c_exchanges=[
-        {'name': "Binance"},
-        {'name': "Coinbase"},
-        {'name': "BitMEX"},
-        {'name': "Huobi Global"},
-        {'name': "KuCoin"}
+        {'name': "Binance",
+         'link': "https://www.binance.com/en?ref=15269217"},
+        {'name': "Coinbase",
+         'link': "https://www.coinbase.com/"},
+        {'name': "BitMEX",
+         'link': "https://www.bitmex.com/"},
+        {'name': "Huobi Global",
+         'link': "https://www.huobi.com/en-us/"},
+        {'name': "KuCoin",
+         'link': "https://www.kucoin.com/"}
         ]
 
-    l_trading = [
-        {'name': "BitMEX"},
-        {'name': "Huobi Global"},
-        {'name': "Binance"},
-        {'name': "Kraken"},
-        {'name': "Phemex"}
-        ]
+    
+    c_news = [
+        {'name': "Cointelegraph",
+         'link': "https://cointelegraph.com/"},
+        {'name': "Coindesk",
+         'link': "https://www.coindesk.com/"},
+        {'name': "NewsBitcoin",
+         'link': "https://news.bitcoin.com/"},
+        {'name': "CCN",
+         'link': "https://www.ccn.com/"},
+        {'name': "BitcoinMagazine",
+         'link': "https://bitcoinmagazine.com/"}
+    ]
 
     c_forums = [
-        {'name': "Bitcointalk"},
-        {'name': "Bitcoin.com forum"},
-        {'name': "Beermoneyforum"},
-        {'name': "Cryptocurrencytalk"},
-        {'name': "Mastersofcrypto Forum"}
+        {'name': "Bitcointalk",
+         'link': "https://bitcointalk.org/"},
+        {'name': "Bitcoin.com forum",
+         'link': "https://forum.bitcoin.com/"},
+        {'name': "Beermoneyforum",
+         'link': "https://www.beermoneyforum.com/"},
+        {'name': "Cryptocurrencytalk",
+         'link': "https://cryptocurrencytalk.com/"},
+        {'name': "Mastersofcrypto Forum",
+         'link': "https://mastersofcrypto.com/forum/"}
         ]
 
+    r_cryptocurrency = [
+        {'name': "r/Bitcoin",
+         'link': "https://www.reddit.com/r/Bitcoin/"},
+        {'name': "r/Cryptocurrency",
+         'link': "https://www.reddit.com/r/Cryptocurrency/"},
+        {'name': "r/ethereum",
+         'link': "https://www.reddit.com/r/ethereum/"},
+        {'name': "r/btc",
+         'link': "https://www.reddit.com/r/btc/"},
+        {'name': "r/litecoin",
+         'link': "https://www.reddit.com/r/litecoin/"}
+    ]
+
+    
+
     b_mining_software = [
-        {'name': "MinerGate"},
-        {'name': "BTCMiner"},
-        {'name': "RPCminer"},
-        {'name': "GUIMiner"},
-        {'name': "GroupFabric"}
+        {'name': "MinerGate",
+         'link': "https://minergate.com/"},
+        {'name': "BTCMiner",
+         'link': "https://mining.bitcoin.com/"},
+        {'name': "RPCminer",
+         'link': "https://en.bitcoin.it/wiki/RPC_Miner"},
+        {'name': "GUIMiner",
+         'link': "https://cnguiminer.com/"},
+        {'name': "GroupFabric",
+         'link': "https://www.groupfabric.com/"}
         ]
 
     m_pools = [
-        {'name': "F2Pool"},
-        {'name': "Antpool"},
-        {'name': "SlushPool"},
-        {'name': "ViaBTC"},
-        {'name': "Bitminter"}
-        ]
-
-    c_tracking = [
-        {'name': "CoinTracking"},
-        {'name': "coin.fyi"},
-        {'name': "Blox"},
-        {'name': "Altpocket"},
-        {'name': "Blockfolio"}
-        ]
-
-    charting = [
-        {'name': "Coinmarketcap"},
-        {'name': "Investing.com"},
-        {'name': "Tradingview"},
-        {'name': "Etherscan.io"},
-        {'name': "Blockchain"}
+        {'name': "F2Pool",
+         'link': "https://www.f2pool.com/"},
+        {'name': "Antpool",
+         'link': "https://antpool.com/"},
+        {'name': "SlushPool",
+         'link': "https://slushpool.com/home/"},
+        {'name': "ViaBTC",
+         'link': "https://www.viabtc.com/?lang=en_US"},
+        {'name': "Bitminter",
+         'link': "https://bitminter.com/"}
         ]
 
     u_ico = [
-        {'name': "Fidelityhouse"},
-        {'name': "Membrana"},
-        {'name': "Poof Of Toss"},
-        {'name': "Elrond"},
-        {'name': "Manda"}
+        {'name': "Fidelityhouse",
+         'link': "https://icobench.com/ico/fidelity-house"},
+        {'name': "Membrana",
+         'link': "https://icobench.com/ico/membrana"},
+        {'name': "Poof Of Toss",
+         'link': "https://icobench.com/ico/toss"},
+        {'name': "Elrond",
+         'link': "https://elrond.com/"},
+        {'name': "Aidus",
+         'link': "https://aidus.io/"}
     ]
 
     airdrops = [
-        {'name': "ICO Drops"},
-        {'name': "Gift.ONE"},
-        {'name': "Airdrop Alert"},
-        {'name': "AirdropBob"},
-        {'name': "Coin Airdrops"}
+        {'name': "ICO Drops",
+         'link': "https://icodrops.com/"},
+        {'name': "Gift.ONE",
+         'link': "https://www.facebook.com/GiftONE2018/"},
+        {'name': "Airdrop Alert",
+         'link': "https://airdropalert.com/"},
+        {'name': "AirdropBob",
+         'link': "https://www.airdropbob.com/"},
+        {'name': "Coin Airdrops",
+         'link': "https://coinairdrops.com/"}
         ]
+
+    c_calender = [
+        {'name': "CoinMarketCal",
+         'link': "https://coinmarketcal.com/en/"},
+        {'name': "ICO Calender",
+         'link': "https://tokenmarket.net/ico-calendar"},
+        {'name': "Coin Calender",
+         'link': "http://www.coincalendar.info/"},
+        {'name': "Wiser ICO",
+         'link': "https://icobench.com/ico/wise"},
+        {'name': "Coins Calender",
+         'link': "https://coinscalendar.com/"}
+    ]
+
+    charting = [
+        {'name': "Coinmarketcap",
+         'link': "https://coinmarketcap.com/"},
+        {'name': "Investing.com",
+         'link': "https://www.investing.com/"},
+        {'name': "Tradingview",
+         'link': "https://in.tradingview.com/"},
+        {'name': "Etherscan.io",
+         'link': "https://etherscan.io/"},
+        {'name': "Blockchain",
+         'link': "https://www.blockchain.com/"}
+        ]
+
+    c_gambling = [
+        {'name': "Luckygames.io",
+         'link': "https://luckygames.io/"},
+        {'name': "bitStarz",
+         'link': "https://www.bitstarz.com/"},
+        {'name': "Nitrogensports",
+         'link': "https://nitrogensports.eu/"},
+        {'name': "Primedice",
+         'link': "https://primedice.com/"},
+        {'name': "mBit Casino",
+         'link': "https://www.mbitcasino.com/"}
+        
+        ]
+
+
+    c_tracking = [
+        {'name': "CoinTracking",
+         'link': "https://cointracking.info/"},
+        {'name': "coin.fyi",
+         'link': "https://coin.fyi/"},
+        {'name': "Blox",
+         'link': "https://www.blox.io/"},
+        {'name': "Altpocket",
+         'link': "https://altpocket.io/"},
+        {'name': "Blockfolio",
+         'link': "https://blockfolio.com/"}
+        ]
+
+    
+
+    
 
     context={
         'c_wallets' : c_wallets,
         'h_wallets' : h_wallets,
-        'c_gambling' : c_gambling,
         'c_exchanges' : c_exchanges,
-        'l_trading' : l_trading,
+        'c_news': c_news,
         'c_forums' : c_forums,
+        'r_cryptocurrency': r_cryptocurrency,
         'b_mining_software' : b_mining_software,
         'm_pools' : m_pools,
-        'c_tracking' : c_tracking,
-        'charting' : charting,
         'u_ico' : u_ico,
-        'airdrops' : airdrops
-
-
+        'airdrops' : airdrops,
+        'c_calender' : c_calender,
+        'charting' : charting,
+        'c_gambling' : c_gambling,
+        'c_tracking' : c_tracking
         
     }
     return render(request,'resources.html',context)
